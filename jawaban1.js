041ba764d1a1cb9c35a5c3933fe64148bc732ec3
Muhamad Ridwan Nullah <73215317+onesrid@users.noreply.github.com>
2021-11-03 22:36:19 +0700

    Update jawaban1.js

--- jawaban1.js
+++ jawaban1.js
@@ -1,5 +1,5 @@
 let Fruits = [
-    ["Apel", "Pisang", "Jambu", "Anggur", "Pepaya", "Duku", "Durian", "Jeruk", "Mangga", "Manggis"]
+    ["Apel"], ["Pisang"], ["Jambu"], ["Anggur"], ["Pepaya"], ["Duku"], ["Durian"], ["Jeruk"], ["Mangga"], ["Manggis"]
 ];
 Fruits.sort(); 
 console.log(Fruits);
