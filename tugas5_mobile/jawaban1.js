35ed5aa7281f9e4effa0ec6ec126b6c00e05cc97
Muhamad Ridwan Nullah <73215317+onesrid@users.noreply.github.com>
2021-11-06 19:49:54 +0700

    Add files via upload

---
+++ tugas5_mobile/jawaban1.js
@@ -0,0 +1,8 @@
+let Fruits = new Array("Apel", "Pisang", "Jambu", "Anggur", "Pepaya", "Duku",
+    "Durian", "Jeruk", "Mangga", "Manggis")
+    
+        
+}
+Fruits.sort(); 
+console.log(box);
+console.log(Fruits);
