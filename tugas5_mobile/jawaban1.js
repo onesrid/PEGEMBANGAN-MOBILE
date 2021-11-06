1a9e1692d9968fa69b02483d935a229018c0a27a
Muhamad Ridwan Nullah <73215317+onesrid@users.noreply.github.com>
2021-11-06 19:54:37 +0700

    Add files via upload

---
+++ tugas5_mobile/jawaban1.js
@@ -0,0 +1,29 @@
+let fruits = new Array ("Apel", "Pisang", "Jambu", "Anggur", "Pepaya", "Duku", "Durian", "Jeruk", "Mangga", "Manggis");
+let boxA = []
+let boxP = []
+let boxJ = []
+let boxD = []
+let boxM = []
+for(let i = 0; i< fruits.length; i++){
+    let hurufAwal = fruits[i].substring(0,1);
+
+    if (hurufAwal == "A") {
+        boxA.push(fruits[i]);
+    }else if (hurufAwal == "P" ) {
+        boxP.push(fruits[i])
+    }else if (hurufAwal == "J") {
+        boxJ.push(fruits[i])
+    }else if (hurufAwal == "D") {
+        boxD.push(fruits[i])
+    }else if (hurufAwal == "M") {
+        boxM.push(fruits[i])
+    }else{
+        console.log("Tidak Ada Data");
+    }
+}
+
+console.log(boxA);
+console.log(boxP);
+console.log(boxJ);
+console.log(boxD);
+console.log(boxM);
