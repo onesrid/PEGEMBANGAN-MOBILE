8ecb1cefb4de883d835d502b235a49f86d510313
Muhamad Ridwan Nullah <73215317+onesrid@users.noreply.github.com>
2021-11-06 19:51:18 +0700

    Delete tugas5_mobile direktory

--- tugas5_mobile/jawaban1.js
+++
@@ -1,8 +0,0 @@
-let Fruits = new Array("Apel", "Pisang", "Jambu", "Anggur", "Pepaya", "Duku",
-    "Durian", "Jeruk", "Mangga", "Manggis")
-    
-        
-}
-Fruits.sort(); 
-console.log(box);
-console.log(Fruits);
